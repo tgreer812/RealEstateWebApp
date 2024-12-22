34ff1a48b960aa06fe644a84afd2bd767f959d84
Tyler Greer <tgreer812@gmail.com>
2024-12-22 12:19:19 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+//import ServerCreator from './ServerCreator';
+//import ServerDisplay from './ServerDisplay';
+
+const App = () => {
+  
+
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default App;
